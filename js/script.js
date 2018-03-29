51ea21b292a153eb7ff730e82ada9632a1972284
Maria <mariagofmanv@gmail.com>
2018-03-29 21:40:08 +0500

    анимация формы

--- js/script.js
+++ js/script.js
@@ -1,32 +1,82 @@
 var bookingBtn = document.querySelector(".booking__btn");
+var searchBtn = document.querySelector(".search__btn");
 var bookingForm = document.querySelector(".booking__form");
-var bookingFormOpened = false;
+var bookingFormClosed = true;
+var bookingArrival = document.querySelector(".booking__input[name=arrival-date]");
+var bookingDeparture = document.querySelector(".booking__input[name=departure-date]");
+var bookingAdults = document.querySelector(".booking__input[name=booking-adults]");
+var bookingChildren = document.querySelector(".booking__input[name=booking-children]");
+var isStorageSupport = true;
 
 
-/*
-function toggleBookingForm() {
-  if (bookingFormOpened) {
-      bookingBtn.classList.remove("visually-hidden");
-        bookingFormOpened = false;
+function fillFromStorage(formFields,isStorageSupport) {
+  try {
+    storageArrival = localStorage.getItem("bookingArrival");
+    storageDeparture = localStorage.getItem("bookingDeparture");
+    storageAdults = localStorage.getItem("bookingAdults");
+    storageChildren = localStorage.getItem("bookingChildren");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  if (storageArrival) {
+    bookingArrival.value = storageArrival;
+  }
+}
+
+bookingFields = [bookingArrival, bookingDeparture, bookingAdults, bookingChildren];
+
+fillFromStorage(bookingFields, isStorageSupport);
+
+bookingBtn.addEventListener("click", function (evt) {
+ evt.preventDefault();
+ if (bookingFormClosed) {
+     /*bookingForm.classList.remove("visually-hidden");*/
+     bookingForm.classList.remove("js-booking__retract");
+     bookingForm.classList.add("js-booking__bounce");
+     bookingArrival.focus();
+   /*if (storage) {
+      login.value = storage;
+      password.focus();
     } else {
-    bookingBtn.classList.add("visually-hidden");
-        bookingFormOpened = true;
-        alert('Test');
+      login.focus();
+    }*/
+     bookingFormClosed = false;
+   } else {
+     /*bookingForm.classList.add("visually-hidden");*/
+     bookingForm.classList.remove("js-booking__bounce");
+     bookingForm.classList.add("js-booking__retract");
+     bookingFormClosed = true;
+   }
+ });
+
+
+/*bookingForm.addEventListener("submit", function (evt) {*/
+  /*почему-то событие с формы не ловится*/
+searchBtn.addEventListener("click", function (evt) {
+  console.log("Нажали сабмит");
+  if (!bookingArrival.value || !bookingDeparture.value || !bookingAdults.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести заезд, отъезд и хотя бы одного взрослого");
+  } else {
+    if (isStorageSupport ) {
+      localStorage.setItem("bookingArrival", bookingArrival.value);
+  		localStorage.setItem("bookingDeparture", bookingDeparture.value);
+  		localStorage.setItem("bookingAdults", bookingAdults.value);
+  		localStorage.setItem("bookingChildren", bookingChildren.value);
     }
-}
-*/
+  }
+});
 
-function handlerBookingBtn(evt) {
-  evt.preventDefault();
-  console.log("test");
-    alert("test Try test");
-}
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!(bookingForm.classList.contains("visually-hidden"))) {
+      /*bookingForm.classList.add("visually-hidden");*/
+      bookingForm.classList.remove("js-booking__bounce");
+      bookingForm.classList.add("js-booking__retract");
+      bookingFormClosed = true;
+    }
+  }
+});
 
-if (bookingBtn) {
-  bookingBtn.addEventListener("click", handlerBookingBtn);
-} else {
-  console.log("test failed");
-    alert('Test failed failed');
-}
-
-/*bookingBtn.addEventListener("click", toggleBookingForm());*/
